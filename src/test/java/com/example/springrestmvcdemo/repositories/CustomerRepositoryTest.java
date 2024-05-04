af7cb9439482ea0328086d5c01c125f1ff0ad173
Jasmin Makwana <jasminmakwana1999@gmail.com>
2024-05-04 15:02:44 +0530

    Added Tests for BeerRepository and CustomerRepository

---
+++ src/test/java/com/example/springrestmvcdemo/repositories/CustomerRepositoryTest.java
@@ -0,0 +1,26 @@
+package com.example.springrestmvcdemo.repositories;
+
+import com.example.springrestmvcdemo.entities.Customer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class CustomerRepositoryTest {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Test
+    void testSaveCustomer() {
+        Customer savedCustomer = customerRepository.save(Customer.builder()
+                        .name("New name")
+                .build());
+
+        assertThat(savedCustomer).isNotNull();
+        assertThat(savedCustomer.getId()).isNotNull();
+    }
+}
