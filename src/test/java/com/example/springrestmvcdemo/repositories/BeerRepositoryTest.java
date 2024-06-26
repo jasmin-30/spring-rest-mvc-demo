f2e0fc2e68f1b4b8cba0768f8f3c4cbf01ab704b
Jasmin Makwana <jasminmakwana1999@gmail.com>
2024-06-26 14:52:51 +0530

    Added Database Constraint Validation

--- src/test/java/com/example/springrestmvcdemo/repositories/BeerRepositoryTest.java
+++ src/test/java/com/example/springrestmvcdemo/repositories/BeerRepositoryTest.java
@@ -2,6 +2,7 @@ package com.example.springrestmvcdemo.repositories;
 
 import com.example.springrestmvcdemo.entities.Beer;
 import com.example.springrestmvcdemo.model.BeerStyle;
+import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -30,5 +31,19 @@ class BeerRepositoryTest {
 
         assertThat(savedBeer).isNotNull();
         assertThat(savedBeer.getId()).isNotNull();
+    }
+
+    @Test
+    void testSaveBeerBeerNameTooLong() {
+        assertThrows(ConstraintViolationException.class, () -> {
+            beerRepository.save(Beer.builder()
+                    .beerName("My Beer 01234560123456012345601234560123456012345601234560123456012345601234560123456012345601234560123456")
+                    .beerStyle(BeerStyle.ALE)
+                    .price(new BigDecimal("11.99"))
+                    .upc("123445")
+                    .build());
+
+            beerRepository.flush();
+        });
     }
 }
