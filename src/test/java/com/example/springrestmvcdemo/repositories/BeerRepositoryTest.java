0d0e0b5fcc085b610c8bcfdc368801ef6539914a
Jasmin Makwana <jasminmakwana1999@gmail.com>
2024-06-26 14:41:00 +0530

    Added Validations at the entity level

--- src/test/java/com/example/springrestmvcdemo/repositories/BeerRepositoryTest.java
+++ src/test/java/com/example/springrestmvcdemo/repositories/BeerRepositoryTest.java
@@ -1,9 +1,12 @@
 package com.example.springrestmvcdemo.repositories;
 
 import com.example.springrestmvcdemo.entities.Beer;
+import com.example.springrestmvcdemo.model.BeerStyle;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -18,7 +21,12 @@ class BeerRepositoryTest {
     void testSaveBeer() {
         Beer savedBeer = beerRepository.save(Beer.builder()
                         .beerName("My Beer")
+                        .beerStyle(BeerStyle.ALE)
+                        .price(new BigDecimal("11.99"))
+                        .upc("123445")
                 .build());
+
+        beerRepository.flush();
 
         assertThat(savedBeer).isNotNull();
         assertThat(savedBeer.getId()).isNotNull();
