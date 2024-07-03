31c1d11b6c4e8d5554d1ad34fb6d8fbeed63a636
Jasmin Makwana <jasminmakwana1999@gmail.com>
2024-07-03 15:40:39 +0530

    Added method to save csv data into database.

--- src/main/java/com/example/springrestmvcdemo/services/BeerCsvServiceImpl.java
+++ src/main/java/com/example/springrestmvcdemo/services/BeerCsvServiceImpl.java
@@ -2,12 +2,14 @@ package com.example.springrestmvcdemo.services;
 
 import com.example.springrestmvcdemo.model.BeerCSVRecord;
 import com.opencsv.bean.CsvToBeanBuilder;
+import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.List;
 
+@Service
 public class BeerCsvServiceImpl implements BeerCsvService {
     @Override
     public List<BeerCSVRecord> convertCSV(File csvFile) {
