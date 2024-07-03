34c5fbf630d391558570662dbdaf8bb3760463da
Jasmin Makwana <jasminmakwana1999@gmail.com>
2024-07-03 16:28:20 +0530

    Added test case to retrieve list of beers by beer name

--- src/test/java/com/example/springrestmvcdemo/repositories/BeerRepositoryTest.java
+++ src/test/java/com/example/springrestmvcdemo/repositories/BeerRepositoryTest.java
@@ -1,22 +1,33 @@
 package com.example.springrestmvcdemo.repositories;
 
+import com.example.springrestmvcdemo.bootstrap.BootStrapData;
 import com.example.springrestmvcdemo.entities.Beer;
 import com.example.springrestmvcdemo.model.BeerStyle;
+import com.example.springrestmvcdemo.services.BeerCsvServiceImpl;
 import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
+@Import({BootStrapData.class, BeerCsvServiceImpl.class})
 class BeerRepositoryTest {
 
     @Autowired
     BeerRepository beerRepository;
+
+    @Test
+    void testBeerListByBeerName() {
+        List<Beer> beerList = beerRepository.findAllByBeerNameIsLikeIgnoreCase("%IPA%");
+        assertThat(beerList.size()).isEqualTo(336);
+    }
 
     @Test
     void testSaveBeer() {
