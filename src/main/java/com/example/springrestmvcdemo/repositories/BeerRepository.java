7c176ff677ae379975e273209b2fdc90b0fbf198
Jasmin Makwana <jasminmakwana1999@gmail.com>
2024-07-03 16:27:58 +0530

    Added JPA Query method to find beers by beer name

--- src/main/java/com/example/springrestmvcdemo/repositories/BeerRepository.java
+++ src/main/java/com/example/springrestmvcdemo/repositories/BeerRepository.java
@@ -3,7 +3,9 @@ package com.example.springrestmvcdemo.repositories;
 import com.example.springrestmvcdemo.entities.Beer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerRepository extends JpaRepository<Beer, UUID> {
+    List<Beer> findAllByBeerNameIsLikeIgnoreCase(String beerName);
 }
