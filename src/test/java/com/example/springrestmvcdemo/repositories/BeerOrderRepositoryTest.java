0606b4f99289e1c93a2571acec8e9fc12882ac22
Jasmin Makwana <jasminmakwana1999@gmail.com>
2024-07-05 14:49:54 +0530

    Updated test cases

--- src/test/java/com/example/springrestmvcdemo/repositories/BeerOrderRepositoryTest.java
+++ src/test/java/com/example/springrestmvcdemo/repositories/BeerOrderRepositoryTest.java
@@ -2,6 +2,7 @@ package com.example.springrestmvcdemo.repositories;
 
 import com.example.springrestmvcdemo.bootstrap.BootStrapData;
 import com.example.springrestmvcdemo.entities.Beer;
+import com.example.springrestmvcdemo.entities.BeerOrder;
 import com.example.springrestmvcdemo.entities.Customer;
 import com.example.springrestmvcdemo.services.BeerCsvServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -34,12 +36,15 @@ class BeerOrderRepositoryTest {
         testBeer = beerRepository.findAll().get(0);
     }
 
+    @Transactional
     @Test
     void testBeerOrders() {
-        System.out.println(customerRepository.count());
-        System.out.println(beerOrderRepository.count());
-        System.out.println(beerRepository.count());
-        System.out.println(testBeer.getBeerName());
-        System.out.println(testCustomer.getName());
+        BeerOrder beerOrder = BeerOrder.builder()
+                .customerRef("Test order")
+                .customer(testCustomer)
+                .build();
+
+        BeerOrder savedBeerOrder = beerOrderRepository.saveAndFlush(beerOrder);
+        System.out.println(savedBeerOrder.getCustomerRef());
     }
 }
