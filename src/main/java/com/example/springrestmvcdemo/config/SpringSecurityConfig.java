892677e5bca046c2ab5c8fd085937f41661c3d16
Jasmin Makwana <jasminmakwana1999@gmail.com>
2024-07-12 15:47:08 +0530

    Added authentication for all requests

--- src/main/java/com/example/springrestmvcdemo/config/SpringSecurityConfig.java
+++ src/main/java/com/example/springrestmvcdemo/config/SpringSecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.springrestmvcdemo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -11,8 +12,10 @@ public class SpringSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(httpSecurityCsrfConfigurer -> {
-            httpSecurityCsrfConfigurer.ignoringRequestMatchers("/api/**");
-        });
+                    httpSecurityCsrfConfigurer.ignoringRequestMatchers("/api/**");
+                })
+                .httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> authorizationManagerRequestMatcherRegistry.anyRequest().authenticated());
 //        http.csrf().ignoringRequestMatchers();
         return http.build();
     }
